bb977ea8b31024410acb6fa4bd7917c831168ed1
Ianny Mamedes <105374233+iannymamedes@users.noreply.github.com>
2022-10-15 23:04:59 -0300

    Add files via upload

---
+++ Vendedor.java
@@ -0,0 +1,48 @@
+public class Vendedor {
+
+    private String nome;
+    private int codigo;
+    private float comissao;
+    private String endereco;
+
+
+    public Vendedor(String nome, int codigo, float comissao, String endereco) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.comissao = comissao;
+        this.endereco = endereco;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public float getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(float comissao) {
+        this.comissao = comissao;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
